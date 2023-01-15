017d4756adb6a28917a0f4eae772e30880ff03d3
Subham Saha <saha007subham@gmail.com>
2023-01-15 19:25:57 +0530

    removed inline styles from app-layout files

--- src/app-layout/AppLayout.js
+++ src/app-layout/AppLayout.js
@@ -2,32 +2,22 @@ import React from "react";
 import Header from "../components/Header";
 import Cards from "../components/Cards";
 import { retroData } from "../data/data";
+import "./AppLayout.css";
 
 const AppLayout = () => {
   return (
     <>
       <Header />
-      <div style={{ padding: "0px 50px" }}>
+      <div className="total_restro-h2">
         <h2>20 Restaurants</h2>
       </div>
-      <div style={{ paddingLeft: "30px", marginBottom: "80px" }}>
-        <div
-          style={{
-            padding: "0px 20px",
-            display: "grid",
-            gridTemplateColumns: "repeat(4, 1fr)",
-            rowGap: "25px",
-          }}
-        >
+
+      {/* Restaurant Cards Sections */}
+      <div className="main__card-div">
+        <div>
           {retroData.map((item, index) => {
             return <Cards {...item} key={index} />;
           })}
-          {/* <Cards data={retroData[0]} />
-        <Cards data={retroData[1]} />
-        <Cards data={retroData[2]} />
-        <Cards data={retroData[3]} />
-        <Cards data={retroData[4]} />
-        <Cards data={retroData[5]} /> */}
         </div>
       </div>
     </>
