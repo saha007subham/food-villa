e470ed9223dd32812622a60ad0a771b88a1e6a57
Subham Saha <saha007subham@gmail.com>
2023-02-12 21:43:57 +0530

    implemented shimmer effect

--- src/components/Shimmer/Shimmer.js
+++ src/components/Shimmer/Shimmer.js
@@ -1,15 +1,19 @@
+import "./Shimmer.css";
+
 const Shimmer = () => {
   return (
-    <h1
-      style={{
-        color: "red",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      Loading........
-    </h1>
+    <>
+      <div className="shimmer-header">
+        <p style={{ color: "lightgray" }}>Loading</p>
+      </div>
+      <div className="shimmer-div">
+        {Array(10)
+          .fill("")
+          .map((div) => (
+            <div className="shimmer-card"></div>
+          ))}
+      </div>
+    </>
   );
 };
 
