7b12d56449feee7e4fa24d070862d3ca4cb88456
Subham Saha <saha007subham@gmail.com>
2023-03-05 22:03:31 +0530

    added instamart page

--- src/components/Instamart/Instamart.js
+++ src/components/Instamart/Instamart.js
@@ -1,8 +1,83 @@
+import { useState } from "react";
+
+const Section = ({ title, discription, isVisible, setIsVisible }) => {
+  // const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="border border-black p-2 m-2">
+      <h3 className="font-bold text-xl">{title}</h3>
+      {isVisible ? (
+        <button
+          className="cursor-pointer underline"
+          onClick={() => setIsVisible(false)}
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          className="cursor-pointer underline"
+          onClick={() => setIsVisible(true)}
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{discription}</p>}
+    </div>
+  );
+};
+
 const Instamart = () => {
+  const [sectionConsfig, setSectionConfig] = useState({
+    showAbout: false,
+    showTeam: false,
+    showCareer: false,
+  });
+
   return (
     <div>
-      <h1>Instamart</h1>
-      <h2>100s of component in it</h2>
+      <h1 className="text-3xl p-2 m-2 font-bold text-center">Instamart</h1>
+      <Section
+        title="About Instamart"
+        discription={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        isVisible={sectionConsfig.showAbout}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: true,
+            showTeam: false,
+            showCareer: false,
+          })
+        }
+      />
+      <Section
+        title="Teams of Instamart"
+        discription={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat"
+        }
+        isVisible={sectionConsfig.showTeam}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeam: true,
+            showCareer: false,
+          })
+        }
+      />
+      <Section
+        title="Career of Instamart"
+        discription={
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus"
+        }
+        isVisible={sectionConsfig.showCareer}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeam: false,
+            showCareer: true,
+          })
+        }
+      />
     </div>
   );
 };
