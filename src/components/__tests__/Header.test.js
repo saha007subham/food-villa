b1a53c6bf63f6beaf006d8c39a99f2e62a30ef69
Subham Saha <saha007subham@gmail.com>
2023-03-11 23:11:45 +0530

    config jest file

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,9 +1,15 @@
 import { render } from "@testing-library/react";
 import Headeer from "../Header";
+import { Provider } from "react-redux";
+import store from "../../utils/store";
 
 test("Logo should load on rendering header", () => {
   // Load Header
-  const header = render(<Headeer />);
+  const header = render(
+    <Provider store={store}>
+      <Headeer />
+    </Provider>
+  );
 
   console.log(header);
   //Check if logo is loaded
